d120306a080227c127b0eb8cea1b62f4390bdff4
Vitalya <vitamin11182005@gmail.com>
2025-01-23 10:41:23 +0300

    add migrations

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS public.songs (
+    id SERIAL PRIMARY KEY,
+    group VARCHAR(255) NOT NULL,
+    song VARCHAR(255) NOT NULL,
+    release_date DATE NOT NULL,
+    text TEXT NOT NULL,
+    link TEXT NOT NULL,
+);
